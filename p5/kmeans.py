a49f2e748e7cea9615276291204fe0df61d4e5b3
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2019-12-23 15:02:32 +0300

    updated

--- p5/kmeans.py
+++ p5/kmeans.py
@@ -2,10 +2,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import copy
 
 x = []
 y = []
+pdf = PdfPages('plot.pdf')
 with open('data.txt','r') as f:
 	next(f)
 	for line in f:
@@ -25,102 +27,108 @@ minx = min(x)
 miny = min(y)
 maxx = max(x)
 maxy = max(y)
-np.random.seed(200)
-k = 3
+colmap = {1: 'r', 2: 'g', 3: 'b', 4:'y', 5:'c'}
+#np.random.seed(200)
+#k = 3
 # centroids[i] = [x, y]
-centroids = {
-    i+1: [np.random.randint(minx, maxx), np.random.randint(miny, maxy)]
-    for i in range(k)
-}
-   
-fig = plt.figure(figsize=(5, 5))
-plt.scatter(df['x'], df['y'], color='k')
-colmap = {1: 'r', 2: 'g', 3: 'b'}
-for i in centroids.keys():
-    plt.scatter(*centroids[i], color=colmap[i])
-plt.xlim(minx,maxx)
-plt.ylim(miny,maxy)
-# plt.show()
+for k in range(2,6):
+	centroids = {
+		i+1: [np.random.randint(minx, maxx), np.random.randint(miny, maxy)]
+		for i in range(k)
+	}
+	   
+	#fig = plt.figure(figsize=(5, 5))
+	#plt.scatter(df['x'], df['y'], color='k')
+	
+	#for i in centroids.keys():
+	#    plt.scatter(*centroids[i], color=colmap[i])
+	#plt.xlim(minx,maxx)
+	#plt.ylim(miny,maxy)
+	#plt.show()
 
 
-## Assignment Stage
-def assignment(df, centroids):
-    for i in centroids.keys():
-        # sqrt((x1 - x2)^2 - (y1 - y2)^2)
-        df['distance_from_{}'.format(i)] = (
-            np.sqrt(
-                (df['x'] - centroids[i][0]) ** 2
-                + (df['y'] - centroids[i][1]) ** 2
-            )
-        )
-    centroid_distance_cols = ['distance_from_{}'.format(i) for i in centroids.keys()]
-    df['closest'] = df.loc[:, centroid_distance_cols].idxmin(axis=1)
-    df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))
-    df['color'] = df['closest'].map(lambda x: colmap[x])
-    return df
+	## Assignment Stage
+	def assignment(df, centroids):
+		for i in centroids.keys():
+			# sqrt((x1 - x2)^2 - (y1 - y2)^2)
+			df['distance_from_{}'.format(i)] = (
+				np.sqrt(
+					(df['x'] - centroids[i][0]) ** 2
+					+ (df['y'] - centroids[i][1]) ** 2
+				)
+			)
+		centroid_distance_cols = ['distance_from_{}'.format(i) for i in centroids.keys()]
+		df['closest'] = df.loc[:, centroid_distance_cols].idxmin(axis=1)
+		df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))
+		df['color'] = df['closest'].map(lambda x: colmap[x])
+		return df
 
-df = assignment(df, centroids)
-print(df.head())
+	df = assignment(df, centroids)
+	print(df.head())
 
-fig = plt.figure(figsize=(5, 5))
-plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
-for i in centroids.keys():
-    plt.scatter(*centroids[i], color=colmap[i])
-plt.xlim(minx,maxx)
-plt.ylim(miny,maxy)
-# plt.show()
+	#fig = plt.figure(figsize=(5, 5))
+	#plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+	#for i in centroids.keys():
+	#    plt.scatter(*centroids[i], color=colmap[i])
+	#plt.xlim(minx,maxx)
+	#plt.ylim(miny,maxy)
+	#plt.show()
 
-##Update Stage
-old_centroids = copy.deepcopy(centroids)
+	##Update Stage
+	old_centroids = copy.deepcopy(centroids)
 
-def update(k):
-    for i in centroids.keys():
-        centroids[i][0] = np.mean(df[df['closest'] == i]['x'])
-        centroids[i][1] = np.mean(df[df['closest'] == i]['y'])
-    return k
+	def update(k):
+		for i in centroids.keys():
+			centroids[i][0] = np.mean(df[df['closest'] == i]['x'])
+			centroids[i][1] = np.mean(df[df['closest'] == i]['y'])
+		return k
 
-centroids = update(centroids)
-    
-fig = plt.figure(figsize=(5, 5))
-ax = plt.axes()
-plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
-for i in centroids.keys():
-    plt.scatter(*centroids[i], color=colmap[i])
-plt.xlim(minx,maxx)
-plt.ylim(miny,maxy)
-for i in old_centroids.keys():
-    old_x = old_centroids[i][0]
-    old_y = old_centroids[i][1]
-    dx = (centroids[i][0] - old_centroids[i][0]) * 0.75
-    dy = (centroids[i][1] - old_centroids[i][1]) * 0.75
-    ax.arrow(old_x, old_y, dx, dy, head_width=2, head_length=3, fc=colmap[i], ec=colmap[i])
-# plt.show()
+	centroids = update(centroids)
+		
+	#fig = plt.figure(figsize=(5, 5))
+	#ax = plt.axes()
+	#plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+	#for i in centroids.keys():
+	#    plt.scatter(*centroids[i], color=colmap[i])
+	#plt.xlim(minx,maxx)
+	#plt.ylim(miny,maxy)
+	#for i in old_centroids.keys():
+	#    old_x = old_centroids[i][0]
+	#    old_y = old_centroids[i][1]
+	#    dx = (centroids[i][0] - old_centroids[i][0]) * 0.75
+	#    dy = (centroids[i][1] - old_centroids[i][1]) * 0.75
+	#    ax.arrow(old_x, old_y, dx, dy, head_width=2, head_length=3, fc=colmap[i], ec=colmap[i])
+	#plt.show()
 
-## Repeat Assigment Stage
-df = assignment(df, centroids)
+	## Repeat Assigment Stage
+	df = assignment(df, centroids)
 
-# Plot results
-fig = plt.figure(figsize=(5, 5))
-plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
-for i in centroids.keys():
-    plt.scatter(*centroids[i], color=colmap[i])
-plt.xlim(minx,maxx)
-plt.ylim(miny,maxy)
-# plt.show()
+	# Plot results
+	#fig = plt.figure(figsize=(5, 5))
+	#plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+	#for i in centroids.keys():
+	#    plt.scatter(*centroids[i], color=colmap[i])
+	#plt.xlim(minx,maxx)
+	#plt.ylim(miny,maxy)
+	#plt.show()
 
-# Continue until all assigned categories don't change any more
-while True:
-    closest_centroids = df['closest'].copy(deep=True)
-    centroids = update(centroids)
-    df = assignment(df, centroids)
-    if closest_centroids.equals(df['closest']):
-        break
+	# Continue until all assigned categories don't change any more
+	while True:
+		closest_centroids = df['closest'].copy(deep=True)
+		centroids = update(centroids)
+		df = assignment(df, centroids)
+		if closest_centroids.equals(df['closest']):
+			break
 
-fig = plt.figure(figsize=(5, 5))
-plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
-for i in centroids.keys():
-    plt.scatter(*centroids[i], color=colmap[i])
-plt.xlim(minx,maxx)
-plt.ylim(miny,maxy)
-# plt.show()
-fig.savefig("plot.pdf")
+	fig = plt.figure(figsize=(5, 5))
+	plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+	for i in centroids.keys():
+		plt.scatter(*centroids[i], color=colmap[i])
+	plt.xlim(minx,maxx)
+	plt.ylim(miny,maxy)
+	plt.title('k = '+str(k))
+	#plt.show()
+	#fig.savefig("plot.pdf")
+	pdf.savefig(fig)
+	plt.close()
+pdf.close()
